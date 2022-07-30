ae16a2c0a060717dc698d37efc88a9813ff9f9c1
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-07-29 20:19:09 -0400

    more control

--- CamUnsnap/Plugin.cs
+++ CamUnsnap/Plugin.cs
@@ -9,12 +9,15 @@ namespace CamUnSnap
     {
         private static GameObject Hook;
         private const string KeybindSectionName = "Keybinds";
+        private const string CameraSettingsSectionName = "Camera Control Settings";
         internal static ConfigEntry<KeyboardShortcut> ToggleCameraSnap;
         internal static ConfigEntry<KeyboardShortcut> CameraMouse;
+        internal static ConfigEntry<float> CameraMoveSpeed;
 
         private void Awake()
         {
             ToggleCameraSnap = Config.Bind(KeybindSectionName, "Toggle Camera Snap", new KeyboardShortcut(KeyCode.C, KeyCode.LeftControl), "Allows you to unsnap the camera at will");
+            CameraMoveSpeed = Config.Bind(CameraSettingsSectionName, "CameraMoveSpeed", 10f, new ConfigDescription("How fast you want the camera to move", new AcceptableValueRange<float>(0.01f, 100f)));
             CameraMouse = Config.Bind(KeybindSectionName, "Switch camera control to mouse", new KeyboardShortcut(KeyCode.Equals), "Lets you contol the camera viewport with the mouse, switch between enabling to pose your character");
 
             Hook = new GameObject("CUS");
